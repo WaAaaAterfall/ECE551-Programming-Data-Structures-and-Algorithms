cd7fffda47923a25b4a1744685b99306d677d02e
sp645 <sp645@duke.edu>
2022-09-16 13:32:13 -0400

    2 assignment 029

--- 029_power_rec/power.c
+++ 029_power_rec/power.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned power(unsigned num, unsigned order) {
-  if (order == 0) {
+unsigned power(unsigned x, unsigned y) {
+  if (y == 0) {
     return 1;
   }
   else {
-    return num * power(num, order - 1);
+    return x * power(x, y - 1);
   }
 }
