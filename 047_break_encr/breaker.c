158bfd51ba61eac6961f38148db31b58ec502369
sp645 <sp645@duke.edu>
2022-09-29 00:06:47 -0400

    1 assignment 47

---
+++ 047_break_encr/breaker.c
@@ -0,0 +1,50 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int getLetterI(double * count, int sum) {
+  double maxFreq = 0;
+  int indexOfMostFre = 0;
+  for (int i = 0; i < 26; i++) {
+    count[i] = count[i] / sum;
+    if (count[i] > maxFreq) {
+      maxFreq = count[i];
+      indexOfMostFre = i;
+    }
+  }
+  return indexOfMostFre;
+}
+
+void calculate_count(int * count, double * record, FILE * f) {
+  int c;
+  while ((c = fgetc(f)) != EOF) {
+    if (isalpha(c)) {
+      (*count)++;
+      int index = (int)c;
+      record[index - 97]++;
+    }
+  }
+}
+
+int main(int argc, char ** argv) {
+  if (argc != 2) {
+    printf("Wrong argument number!\n");
+    return EXIT_FAILURE;
+  }
+  FILE * f = fopen(argv[1], "r");
+  double count_record[26] = {0};
+  int count = 0;
+  int * c = &count;
+  if (f == NULL) {
+    perror("Could not open file\n");
+    return EXIT_FAILURE;
+  }
+  calculate_count(c, count_record, f);
+  if (count == 0) {
+    printf("Empty File\n");
+    return EXIT_FAILURE;
+  }
+  int indexOfMostFre = getLetterI(count_record, count);
+  printf("%d", (indexOfMostFre - 4));
+  return EXIT_SUCCESS;
+}
