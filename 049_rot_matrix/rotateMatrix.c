9ac8f89e0a80329ac8962198ce075e1872595246
sp645 <sp645@duke.edu>
2022-10-02 17:26:37 -0400

    1 assignment 049

--- 049_rot_matrix/rotateMatrix.c
+++ 049_rot_matrix/rotateMatrix.c
@@ -2,34 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 
-void rotate_matrix(char ** matrix, FILE * f) {
-  int c;
-  while ((c = fgetc(f)) != EOF) {
-    if (c != '\n') {
-      for (int i = 9; i >= 0; i--) {
-        for (int j = 0; j < 10; j++) {
-          char * row = matrix[j];
-          row[i] = c;
-        }
-      }
-    }
-  }
-}
-
 int main(int argc, char ** argv) {
   if (argc != 2) {
     fprintf(stderr, "Useage: encrypt key inputFileName\n");
     return EXIT_FAILURE;
   }
-  if (argv[1] == NULL) {
-    return EXIT_FAILURE;
-  }
   FILE * f = fopen(argv[1], "r");
+  FILE * g = fopen(argv[1], "r");
   if (f == NULL) {
     perror("Could not open file\n");
     return EXIT_FAILURE;
   }
-  char * matrix[10];
+  char row0[11] = {-1};
+  char row1[11] = {-1};
+  char row2[11] = {-1};
+  char row3[11] = {-1};
+  char row4[11] = {-1};
+  char row5[11] = {-1};
+  char row6[11] = {-1};
+  char row7[11] = {-1};
+  char row8[11] = {-1};
+  char row9[11] = {-1};
+  char * matrix[10] = {row0, row1, row2, row3, row4, row5, row6, row7, row8, row9};
+  char matr[10][12] = {0};
+  char revert[10][11] = {0};
   int c;
   int row_count = 0;
   int line_count = 0;
@@ -60,13 +56,31 @@ int main(int argc, char ** argv) {
     fprintf(stderr, "The num of lines is smaller than 10.\n");
     return EXIT_FAILURE;
   }
-  rotate_matrix(matrix, f);
+  c = fgetc(g);
   for (int i = 0; i < 10; i++) {
-    char * line = matrix[i];
     for (int j = 0; j < 10; j++) {
-      printf("%c", line[j]);
+      while (c == '\0' || c == '\n') {
+        c = fgetc(g);
+      }
+      matr[i][j] = c;
+      c = fgetc(g);
     }
-    printf("\n");
+  }
+  //rotate_matrix(matrix, g);
+  if (fclose(f) != 0 || fclose(g) != 0) {
+    perror("Failed to close the input File");
+    return EXIT_FAILURE;
+  }
+  for (int m = 0; m < 10; m++) {
+    for (int n = 0; n < 10; n++) {
+      revert[n][9 - m] = matr[m][n];
+    }
+  }
+  for (int p = 0; p < 10; p++) {
+    for (int q = 0; q < 10; q++) {
+      fprintf(stdout, "%c", revert[p][q]);
+    }
+    fprintf(stdout, "%c", '\n');
   }
   return EXIT_SUCCESS;
 }
