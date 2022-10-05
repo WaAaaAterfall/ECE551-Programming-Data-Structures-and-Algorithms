1533928ae928ffaf32e3d6f66264990dc150ad19
sp645 <sp645@duke.edu>
2022-10-04 22:27:25 -0400

    1 assignment 53

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -14,8 +14,60 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+void printData(char ** record, size_t lineCount) {
+  size_t i = 0;
+  while (i < lineCount) {
+    printf("%s", record[i]);
+    free(record[i]);
+    i++;
+  }
+}
+
+void sortFile(FILE * f) {
+  size_t countChar = 0;
+  size_t countLine = 0;
+  char ** record = NULL;
+  record = malloc(sizeof(*record));
+  char * line = NULL;
+  while (getline(&line, &countChar, f) >= 0) {
+    countLine++;
+    record = realloc(record, (countLine) * sizeof(*record));
+    *(record + (countLine - 1)) = line;
+    line = NULL;
+  }
+  free(line);
+  sortData(record, countLine);
+  printData(record, countLine);
+  free(record);
+}
+
 int main(int argc, char ** argv) {
   //WRITE YOUR CODE HERE!
-
+  if (argc == 1) {
+    FILE * f = stdin;
+    if (f == NULL) {
+      perror("Could not open file\n");
+      return EXIT_FAILURE;
+    }
+    sortFile(f);
+  }
+  else if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
+      FILE * f = fopen(argv[i], "r");
+      if (f == NULL) {
+        perror("Cannot open file!\n");
+        return EXIT_FAILURE;
+      }
+      sortFile(f);
+      if (fclose(f) != 0) {
+        fprintf(stderr, "File to close the input file\n");
+        return EXIT_FAILURE;
+      }
+    }
+  }
+  else {
+    fprintf(stderr, "Wrong argc\n");
+    return EXIT_FAILURE;
+  }
   return EXIT_SUCCESS;
 }
