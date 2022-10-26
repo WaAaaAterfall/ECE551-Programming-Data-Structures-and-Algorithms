7a4dc36f9b118a1cc9e14246e051d2fb9129c6c7
sp645 <sp645@duke.edu>
2022-10-26 19:37:11 -0400

    1

--- 075_int_matrix/IntMatrix.cpp
+++ 075_int_matrix/IntMatrix.cpp
@@ -1,40 +1,99 @@
 #include "IntMatrix.h"
 
-IntMatrix::IntMatrix(){
+IntMatrix::IntMatrix() : numRows(0), numColumns(0), rows(NULL) {
 }
-IntMatrix::IntMatrix(int r, int c) {
-}
-IntMatrix::IntMatrix(const IntMatrix & rhs)  {
 
+IntMatrix::IntMatrix(int r, int c) : numRows(r), numColumns(c) {
+  rows = new IntArray *[numRows];
+  for (int i = 0; i < numRows; i++) {
+    rows[i] = new IntArray(numColumns);
+  }
+}
+
+IntMatrix::IntMatrix(const IntMatrix & rhs) :
+    numRows(rhs.numRows), numColumns(rhs.numColumns) {
+  rows = new IntArray *[numRows];
+  for (int i = 0; i < numRows; i++) {
+    rows[i] = new IntArray(rhs[i]);
+  }
 }
 IntMatrix::~IntMatrix() {
-
+  for (int i = 0; i < numRows; i++) {
+    delete rows[i];
+  }
+  delete[] rows;
 }
-IntMatrix &IntMatrix::operator=(const IntMatrix & rhs) {
 
+IntMatrix & IntMatrix::operator=(const IntMatrix & rhs) {
+  if (this != &rhs) {
+    for (int i = 0; i < numRows; i++) {
+      delete rows[i];
+    }
+    delete[] rows;
+    numRows = rhs.numRows;
+    numColumns = rhs.numColumns;
+    rows = new IntArray *[rhs.numRows];
+    for (int i = 0; i < numRows; i++) {
+      rows[i] = new IntArray(rhs[i]);
+    }
+  }
+  return *this;
 }
+
 int IntMatrix::getRows() const {
-
+  return numRows;
 }
 int IntMatrix::getColumns() const {
-
+  return numColumns;
 }
+
 const IntArray & IntMatrix::operator[](int index) const {
-
+  assert(index >= 0);
+  assert(index < numRows);
+  return *rows[index];
 }
-IntArray & IntMatrix::operator[](int index){
 
+IntArray & IntMatrix::operator[](int index) {
+  assert(index >= 0);
+  assert(index < numRows);
+  return *rows[index];
 }
-
-
 
 bool IntMatrix::operator==(const IntMatrix & rhs) const {
-
+  if (numRows != rhs.numRows) {
+    return false;
+  }
+  if (numColumns != rhs.numColumns) {
+    return false;
+  }
+  for (int i = 0; i < numRows; i++) {
+    if (*rows[i] != *rhs.rows[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 IntMatrix IntMatrix::operator+(const IntMatrix & rhs) const {
-
+  assert(numRows == rhs.numRows);
+  assert(numColumns == rhs.numColumns);
+  IntMatrix ans(numRows, numColumns);
+  for (int i = 0; i < numRows; i++) {
+    for (int j = 0; j < numColumns; j++) {
+      ans[i][j] = rhs[i][j] + (*this)[i][j];
+    }
+  }
+  return ans;
 }
 
 std::ostream & operator<<(std::ostream & s, const IntMatrix & rhs) {
+  s << "[ ";
+  for (int i = 0; i < rhs.getRows(); i++) {
+    s << rhs[i];
+    if (i != rhs.getRows() - 1) {
+      s << ",\n";
+    }
+  }
+  s << " ]";
+  return s;
 }
