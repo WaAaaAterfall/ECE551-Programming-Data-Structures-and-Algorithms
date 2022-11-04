a009c4fbe7e43f158ea7d8ae8440042b39d3b266
sp645 <sp645@duke.edu>
2022-11-04 00:47:14 -0400

    1

--- 088_read_vec/code.cpp
+++ 088_read_vec/code.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
 #include <vector>
 
 template<typename T>
-void f (std::vector<T> & v, const T & d) {
+void f(std::vector<T> & v, const T & d) {
   typename std::vector<T>::iterator it = v.begin();
   while (it != v.end()) {
     if (*it >= d) {
@@ -12,9 +12,10 @@ void f (std::vector<T> & v, const T & d) {
     ++it;
   }
   v.insert(it, d);
-} 
+}
+
 template<typename T>
-void g (std::vector<T> & v, const T & a, const T & b) {
+void g(std::vector<T> & v, const T & a, const T & b) {
   typename std::vector<T>::iterator it = v.begin();
   while (it != v.end()) {
     if (*it % a == b) {
@@ -25,14 +26,15 @@ void g (std::vector<T> & v, const T & a, const T & b) {
     }
   }
 }
+
 int main(void) {
   std::vector<int> someNums;
   for (int i = 0; i < 5; i++) {
-    someNums.push_back (i*i);
+    someNums.push_back(i * i);
   }
   f<int>(someNums, 12);
   f<int>(someNums, 42);
-  g<int>(someNums, 5 , 4);
+  g<int>(someNums, 5, 4);
   std::vector<int>::const_reverse_iterator ri = someNums.rbegin();
   while (ri != someNums.rend()) {
     std::cout << *ri << "\n";
