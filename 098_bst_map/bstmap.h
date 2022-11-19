8ec2db11b86e6b9308a50190f4e1f7e7bbcb4a4f
sp645 <sp645@duke.edu>
2022-11-18 22:46:38 -0500

    333

--- 098_bst_map/bstmap.h
+++ 098_bst_map/bstmap.h
@@ -20,7 +20,36 @@ class BstMap : public Map<K, V> {
 
  public:
   BstMap() : root(NULL){};
-  BstMap & operator=(const BstMap & rhs);
+  BstMap(const BstMap & rhs) { copyHelper(root, rhs.root); }
+  void copyHelper(Node * current, const Node * other) {
+    if (other == NULL) {
+      current = NULL;
+    }
+    else {
+      current = new Node(other->key, other->value);
+      copyHelper(current->left, other->left);
+      copyHelper(current->right, current->right);
+    }
+  }
+
+  BstMap & operator=(const BstMap & rhs) {
+    BstMap temp(rhs);
+    if (this != &rhs) {
+      std::swap(root, temp.root);
+    }
+    return *this;
+  }
+
+  virtual ~BstMap() { destoryBST(root); }
+  void destoryBST(Node * node) {
+    if (node == NULL) {
+      return;
+    }
+    destoryBST(node->left);
+    destoryBST(node->right);
+    delete node;
+  }
+
   virtual void add(const K & key, const V & value) {
     if (root == NULL) {
       Node * newNode = new Node(key, value);
@@ -28,9 +57,6 @@ class BstMap : public Map<K, V> {
     }
     root = addNode(root, key, value);
   }
-  virtual const V & lookup(const K & key) const throw(std::invalid_argument);
-  virtual void remove(const K & key);
-  virtual ~BstMap();
   Node * addNode(Node * node, const K & key, const V & value) {
     if (node == NULL) {
       Node * toAdd = new Node(key, value);
@@ -50,87 +76,76 @@ class BstMap : public Map<K, V> {
     }
     return node;
   }
-  void destoryBST(Node * toDestory);
-};
 
-template<typename K, typename V>
-BstMap<K, V> & BstMap<K, V>::operator=(const BstMap & rhs) {
-  BstMap<K, V> temp(rhs);
-  if (this != &rhs) {
-    std::swap(root, temp.root);
-  }
-  return *this;
-}
-
-template<typename K, typename V>
-BstMap<K, V>::~BstMap() {
-  destoryBST(root);
-}
-
-template<typename K, typename V>
-const V & BstMap<K, V>::lookup(const K & key) const throw(std::invalid_argument) {
-  Node * current = root;
-  while (current != NULL) {
-    if (current->key == key) {
-      V * ans = &(current->value);
-      return *ans;
-    }
-    else if (current->key < key) {
-      current = current->right;
-    }
-    else {
-      current = current->left;
-    }
-  }
-  throw std::invalid_argument("Cannot find the key.\n");
-}
-
-template<typename K, typename V>
-void BstMap<K, V>::destoryBST(Node * toDestory) {
-  if (toDestory == NULL) {
-    return;
-  }
-  destoryBST(toDestory->left);
-  destoryBST(toDestory->right);
-  toDestory->left = NULL;
-  toDestory->right = NULL;
-  delete toDestory;
-}
-
-template<typename K, typename V>
-void BstMap<K, V>::remove(const K & key) {
-  Node ** current = &root;
-  while (*current != NULL) {
-    if (key == (*current)->key) {
-      if ((*current)->left == NULL) {
-        Node * toDelete = *current;
-        *current = (*current)->right;
-        delete toDelete;
+  virtual const V & lookup(const K & key) const throw(std::invalid_argument) {
+    Node * current = root;
+    while (current != NULL) {
+      if (current->key == key) {
+        V * ans = &(current->value);
+        return *ans;
       }
-      else if ((*current)->right == NULL) {
-        Node * toDelete = *current;
-        *current = (*current)->left;
-        delete toDelete;
+      else if (current->key < key) {
+        current = current->right;
       }
       else {
-        Node ** leftMost = &((*current)->left);
-        while ((*leftMost)->right != NULL) {
-          leftMost = &(*leftMost)->right;
-        }
-        Node * left = (*leftMost)->left;
-        (*leftMost)->left = (*current)->left;
-        (*leftMost)->right = (*current)->right;
-        Node * toDelete = *current;
-        *current = *leftMost;
-        *leftMost = left;
-        delete toDelete;
+        current = current->left;
       }
     }
-    else if ((*current)->key < key) {
-      current = &((*current)->right);
+    throw std::invalid_argument("Cannot find the key.\n");
+  }
+
+  virtual void remove(const K & key) {
+    Node * current = root;
+    if (current == NULL) {
+      return;
     }
-    else {
-      current = &((*current)->left);
+    while (current != NULL) {
+      if (key < current->key) {
+        if (current->left != NULL && current->left->key == key) {
+          current->left = removeNode(current->left);
+        }
+        else {
+          current = current->left;
+        }
+      }
+      else {
+        if (current->right != NULL && current->right->key == key) {
+          current->right = removeNode(current->right);
+        }
+        else {
+          current = current->right;
+        }
+      }
     }
   }
-}
+
+  Node * removeNode(Node * node) {
+    if (node->left == NULL) {
+      Node * res = node->right;
+      delete node;
+      return res;
+    }
+    else if (node->right == NULL) {
+      Node * res = node->left;
+      delete node;
+      return res;
+    }
+    else {
+      Node * right = node->right;
+      Node * leftMost;
+      if (node->left->right == NULL) {
+        leftMost = node->left;
+      }
+      else {
+        Node * curr = node->left;
+        while (curr->right != NULL) {
+          curr = curr->right;
+        }
+        leftMost = curr;
+      }
+      leftMost->right = right;
+      delete node;
+      return leftMost->left;
+    }
+  }
+};
