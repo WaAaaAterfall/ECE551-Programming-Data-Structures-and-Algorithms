d96dd20906041c6dc9b1271f2e0945492d16771e
sp645 <sp645@duke.edu>
2022-11-23 23:12:59 -0500

    1

--- 104_huff_map/node.cpp
+++ 104_huff_map/node.cpp
@@ -1,5 +1,17 @@
 #include "node.h"
-void Node::buildMap(BitString b, std::map<unsigned,BitString> & theMap) {
-  //WRITE ME!
-}
 
+void Node::buildMap(BitString b, std::map<unsigned, BitString> & theMap) {
+  //WRITE ME!
+  if (sym != NO_SYM) {
+    assert(left == NULL && right == NULL);
+    theMap[sym] = b;
+  }
+  else {
+    if (left != NULL) {
+      left->buildMap(b.plusZero(), theMap);
+    }
+    if (right != NULL) {
+      right->buildMap(b.plusOne(), theMap);
+    }
+  }
+}
