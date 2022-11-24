29bdbf03ae3f1bc29fd1f8728a295efe70ffe4a6
sp645 <sp645@duke.edu>
2022-11-23 22:56:03 -0500

    1

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -1,5 +1,21 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  priority_queue_t priotoryQueue;
+  for (size_t i = 0; i < 257; i++) {
+    if (counts[i] != 0) {
+      Node * newNode = new Node(i, counts[i]);
+      priotoryQueue.push(newNode);
+    }
+  }
+  while (priotoryQueue.size() > 1) {
+    Node * first = priotoryQueue.top();
+    priotoryQueue.pop();
+    Node * second = priotoryQueue.top();
+    priotoryQueue.pop();
+    Node * merge = new Node(first, second);
+    priotoryQueue.push(merge);
+  }
+  return priotoryQueue.top();
+}
